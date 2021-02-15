77de8911534f66dcbfe770c5b245e271d0f6538d
dimas1997 <dimas6597@gmail.com>
2021-02-15 13:39:48 +0700

    start tugas

--- tugas/webpack.config.js
+++ tugas/webpack.config.js
@@ -15,11 +15,25 @@ module.exports = {
     contentBase: './webapp//www',
     port: 7000,
   },
+  plugins: [
+    new DotenvWebpackPlugin({
+      path: './.env',
+      safe: true,
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
