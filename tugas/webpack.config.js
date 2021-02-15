ed218a686377438d2fd78b8e8f209a0a72645305
Muhammad Achsan Hujjatul Islam <achsanh@gmail.com>
2021-02-15 04:24:56 -0500

    [Checkpoint 1] Convert WebApp to TS

--- tugas/webpack.config.js
+++ tugas/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    tasks: './webapp/src/tasks/main.js',
+    tasks: './webapp/src/tasks/main.ts',
     worker: './webapp/src/worker/main.js',
     performance: './webapp/src/performance/main.js',
   },
@@ -15,12 +15,12 @@ module.exports = {
     contentBase: './webapp//www',
     port: 7000,
   },
-  plugins: [
-    new DotenvWebpackPlugin({
-      path: './.env',
-      safe: true,
-    }),
-  ],
+  // plugins: [
+  //   new DotenvWebpackPlugin({
+  //     path: './.env',
+  //     safe: true,
+  //   }),
+  // ],
   resolve: {
     extensions: ['.js', '.ts'],
   },
